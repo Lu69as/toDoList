728928de449a881a0a0f8048c9eba12d345096a2
Lu69as <lukasokken@gmail.com>
2025-02-11 12:46:09 +0100

    Holy scheisse, this is good code. Trust

--- script.js
+++ script.js
@@ -1,229 +1,183 @@
-if (localStorage.getItem("items") == null || localStorage.getItem("items") == "")
-    localStorage.setItem("items", "Do this§You Should do this, Its probably very important§19. 11. 2024|Or do this§Its also important, I think...")
+let obj = [ { title: "New Note", content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur nemo quia, ipsum officiis sint obcaecati deserunt porro quos odit nisi consequuntur, vel hic facilis, sapiente iusto omnis. Harum, dolorum vel." } ];
+if (!localStorage.getItem("items"))
+    localStorage.setItem("items", JSON.stringify(obj));
+
 
 function activeItem(newItem) {
-    newItem.addEventListener("click", () => {
-        document.querySelectorAll("nav tr").forEach((e) => e.classList.remove("activeItem"));
-        newItem.classList.add("activeItem");
-
-        document.querySelector(".item .title").innerHTML = document.querySelector(".activeItem").firstElementChild.innerHTML;
-        document.querySelector(".item .desc").innerHTML = document.querySelector(".activeItem").lastElementChild.innerHTML;
-    });
-    document.querySelectorAll(".desc input").forEach((e) => { e.addEventListener("click", saveList) });
+    
 };
 
-function saveList() {
-    try {
-        document.querySelector(".activeItem").firstElementChild.innerHTML = document.querySelector(".item .title").innerHTML;
-        document.querySelector(".activeItem").lastElementChild.innerHTML = document.querySelector(".item .desc").innerHTML;
+function init( list ) {
+    list.forEach((item) => {
+        let newItem = document.createElement("tr");
+        newItem.innerHTML = `<td>${ item.title }</td><td class="content">${ item.content }</td>
+            <i class="fa-solid fa-trash" onClick="this.parentElement.remove(); saveList()"></i>`;
+        activeItem(newItem);
+
+        document.querySelector(".general-items").appendChild(newItem);
+    });
+} init( JSON.parse(localStorage.getItem("items")) );
+
+
+// Code to add new notes
+{
+    function addItem() {
+        let value = document.querySelector(".createItem input").value;
+        if (value.length < 1) return;
+        init([ { "title": value, "content": "Temorary text, edit with the pen tool on the left" } ]);
+        saveList();
     }
-    catch (err) { location.reload(); }
-
-    let items = "";
-    document.querySelectorAll("nav table > tr, nav tbody > tr").forEach((tr) => {
-        tr.querySelectorAll("td").forEach((td) => {
-            items += td.innerHTML;
-            
-            if (td.innerHTML != td.parentElement.lastElementChild.innerHTML || tr.parentElement.parentElement.firstElementChild.nodeName == "THEAD")
-                items += "§";
-        })
-
-        if (tr.parentElement.parentElement.firstElementChild.nodeName == "THEAD")
-            items += tr.parentElement.parentElement.firstElementChild.innerText.trimEnd(" ");
-
-
-        if (tr.innerHTML != document.querySelectorAll("nav table > tr, nav tbody > tr")[
-                document.querySelectorAll("nav table > tr, nav tbody > tr").length - 1 ].innerHTML)
-            items += "|";
-    });
-
-    localStorage.setItem("items", items);
+    
+    document.querySelector(".createItem button").addEventListener("click", addItem);
+    document.querySelector(".createItem input").addEventListener("change", addItem);
 }
-
-function init(string) {
-    string.split("|").forEach((item) => {
-        let itemChildren = item.split("§");
-    
-        if (itemChildren[2] != undefined) {
-            let madeChild = false;
-            document.querySelectorAll(".calendar-date").forEach((date) => {
-                if (date.querySelector("th").innerHTML == itemChildren[2]) {
-                    let newItem = document.createElement("tr");
-                    newItem.innerHTML = `<td>${itemChildren[0]}</td><td>${itemChildren[1]}</td>`;
-                    activeItem(newItem);
-    
-                    date.lastElementChild.appendChild(newItem);
-                    madeChild = true;
-                }
-            })
-            if (!madeChild) {
-                let newTable =  document.createElement("table");
-                newTable.classList.add("calendar-date");
-                newTable.innerHTML = `<thead><th>${itemChildren[2]}</th></thead><tbody></tbody>`;
-                    
-                let newItem = document.createElement("tr");
-                newItem.innerHTML = `<td>${itemChildren[0]}</td><td>${itemChildren[1]}</td>`;
-                activeItem(newItem);
-    
-                newTable.lastElementChild.appendChild(newItem);
-                document.querySelector("nav").appendChild(newTable);
-            }
-        }
-        else {
-            let newItem = document.createElement("tr");
-            newItem.innerHTML = `<td>${itemChildren[0]}</td><td>${itemChildren[1]}</td>`;
-            activeItem(newItem);
-    
-            document.querySelector(".general-items").appendChild(newItem);
-        };
-    });
-} init(localStorage.getItem("items"));
-
-function addItem() {
-    let value = document.querySelector(".createItem input").value;
-    if (value.length < 1) return;
-
-    let newItem = document.createElement("tr");
-    newItem.innerHTML = `<td>${value}</td><td>Temorary text, edit with the pen tool on the left</td>`;
-
-    activeItem(newItem, value, "Temorary text, edit with the pen tool on the left");
-
-    document.querySelector(".general-items").appendChild(newItem);
-}
-
-document.querySelector(".createItem button").addEventListener("click", addItem);
-document.querySelector(".createItem input").addEventListener("change", addItem);
 
 {
     document.querySelector(".openMenu").addEventListener("click", () => document.querySelector("nav").classList.toggle("closed") );
     
-    document.querySelector(".deleteItem").addEventListener("click", () => {
-        let m = "Are you sure you want to delete this note?"
-        if (confirm(m)) {
-            document.querySelector(".activeItem").remove();
-            document.querySelectorAll("nav table tr:not(:has(th))")[0].click();
-            saveList();
-        }
-    })
-    
-    document.querySelector(".editItem").addEventListener("click", () => {
-        if (document.querySelector(".title").getAttribute("contenteditable") == "false" || document.querySelector(".title").getAttribute("contenteditable") == null) {
-            document.querySelector(".title").setAttribute("contenteditable", "true");
-            document.querySelector(".desc").setAttribute("contenteditable", "true");
-            document.querySelector(".item").classList.add("edit");
-        }
-        else {
-            document.querySelector(".title").setAttribute("contenteditable", "false");
-            document.querySelector(".desc").setAttribute("contenteditable", "false");
-            document.querySelector(".item").classList.remove("edit");
-    
-            saveList();
-        };
+    document.querySelector(".lock").addEventListener("click", () => {
+        let editable = new Boolean(document.querySelector(".title").getAttribute("contenteditable") == "false");
+        document.querySelectorAll(".title, .desc").forEach((e) => e.setAttribute("contenteditable", editable.toString()));
+        document.body.classList.toggle("locked");
     });
 }
 
 
 document.querySelectorAll("nav table tr:not(:has(th))")[0].click();
 
-setInterval(() => { if (document.querySelector(".switch input").checked) saveList(); }, 1000);
+
+// Function to save everything and an autosave
+{
+    function saveList() {
+        try {
+            document.querySelector(".activeItem").firstElementChild.innerHTML = document.querySelector(".item .title").innerHTML;
+            document.querySelector(".activeItem").querySelector(".content").innerHTML = document.querySelector(".item .desc").innerHTML;
+        }
+        catch (err) { location.reload(); }
+    
+        let items = [];
+        document.querySelectorAll("nav table > tr, nav tbody > tr").forEach((tr) => {
+            trItem = { title: "", content: "" };
+            trItem.title = tr.firstElementChild.innerHTML;
+            trItem.content = tr.querySelector(".content").innerHTML;
+            items.push(trItem);
+        });
+    
+        localStorage.setItem("items", JSON.stringify(items));
+    }
+    document.querySelectorAll(".title, .desc").forEach((e) => 
+        e.addEventListener("input", () => { if (document.querySelector(".autoSave input").checked) saveList() }));
+};
+
+
+document.querySelectorAll(".general-items tr").forEach((e) => e.addEventListener("mousedown", () => {
+    let hasHeldTooLong = false;
+    setTimeout(() => hasHeldTooLong = true, 200);
+
+    e.classList.add("moving");
+    let position = e.getBoundingClientRect();
+    function handleMouseMove() {
+        e.setAttribute("style", `
+            transform: translateY(${event.clientY - position.y - 18}px);
+            z-index: +2; `);
+    };
+
+    function handleMouseUp() {
+        document.removeEventListener("mousemove", handleMouseMove);
+
+        let itemOver = e;
+        let itemOverBy = 1000;
+        document.querySelectorAll(".general-items tr").forEach((tr) => {
+            let trDiff = tr.getBoundingClientRect().y - e.getBoundingClientRect().y;
+            if (trDiff > 0 && trDiff < itemOverBy) {
+                itemOver = tr;
+                itemOverBy = trDiff;
+            }
+        });
+        if (e == itemOver && e.parentElement.lastElementChild != e)
+            e.parentElement.appendChild(e.parentNode.removeChild(e));
+
+        else if (e != itemOver)
+            e.parentElement.insertBefore(e.parentNode.removeChild(e), itemOver)
+
+        e.removeAttribute("style");
+        e.classList.remove("moving");
+
+        if (!hasHeldTooLong) {
+            document.querySelectorAll("nav tr").forEach((tr) => tr.classList.remove("activeItem"));
+            e.classList.add("activeItem");
+
+            document.querySelector(".item .title").innerHTML = document.querySelector(".activeItem").firstElementChild.innerHTML;
+            document.querySelector(".item .desc").innerHTML = document.querySelector(".activeItem").querySelector(".content").innerHTML;
+        }
+
+        document.removeEventListener("mouseup", handleMouseUp);
+        saveList();
+    }
+    document.addEventListener("mousemove", handleMouseMove)
+    document.addEventListener("mouseup", handleMouseUp)
+}));
+
+
+
+document.querySelectorAll("nav .createItem button").forEach((e) => {
+    e.addEventListener("click", (evt) => {
+        switch (evt.target.classList[0]) {
+            case "import":
+                let list;
+                navigator.clipboard.readText()
+                .then(text => { list = text })
+                .catch(err => { list = prompt('Failed to read clipboard contents, paste your code under: ', err); });
+
+                if (list.length <= 2) break;
+                document.querySelectorAll(".general-items tr").forEach((tr) => tr.remove());
+                init( JSON.parse( list ) );
+            break;
+            case "export":
+                let items = [];
+                document.querySelectorAll("nav table > tr, nav tbody > tr").forEach((tr) => {
+                    trItem = { title: tr.firstElementChild.innerHTML, content: tr.lastElementChild.innerHTML };
+                    items.push(trItem);
+                });
+
+                if (!navigator.clipboard)
+                    alert("Copying failed, try to import to a different session manually with the following code: " + items);
+                else {
+                    navigator.clipboard.writeText(JSON.stringify(items))
+                    alert("Copy successfull, go to a different session and hit import to get your list.");
+                }
+            break;
+        };
+    });
+});
 
 document.querySelectorAll(".editButtons button").forEach((e) => {
     e.addEventListener("click", () => {
         let newList;
-        if (e.classList[0] == "list") {
-            newList = document.createElement("ul");
-            newList.innerHTML = `<li></li>`;
+        switch (e.classList[0]) {
+            case "list":
+                newList = document.createElement("ul");
+                newList.innerHTML = `<li></li>`;
+            break;
+            case "numlist":
+                newList = document.createElement("ol");
+                newList.innerHTML = `<li></li>`;
+            break;
+            case "checklist":
+                let c = document.querySelectorAll(".item .desc .input").length;
+                newList = document.createElement("div");
+                newList.innerHTML = `<input type="checkbox" id="box-${c}"><label for="box-${c}"> This is not checked</label>`;
+                newList.firstElementChild.setAttribute("onClick", `this.hasAttribute("checked") 
+                    ? this.removeAttribute("checked") : this.setAttribute("checked", true);`);
+            break;
         }
-        else if (e.classList[0] == "numlist") {
-            newList = document.createElement("ol");
-            newList.innerHTML = `<li></li>`;
-        }
-        else if (e.classList[0] == "checklist") {
-            let c = document.querySelectorAll(".item .desc .input").length;
-            newList = document.createElement("div");
-            newList.innerHTML = `<input type="checkbox" id="box-${c}"><label for="box-${c}"> This is not checked</label>`;
-            newList.firstElementChild.setAttribute("onClick", `this.hasAttribute("checked") 
-                ? this.removeAttribute("checked") : this.setAttribute("checked", true);`);
-            }
-            
+
         if (document.querySelector(".desc").lastElementChild.nodeName == "BR" || 
             (document.querySelector(".desc").lastElementChild.firstElementChild.nodeName == "BR" && 
-                document.querySelector(".desc").lastElementChild.lastElementChild.nodeName == "BR")) {
-                    document.querySelector(".desc").lastElementChild.remove()
+            document.querySelector(".desc").lastElementChild.lastElementChild.nodeName == "BR")) {
+                document.querySelector(".desc").lastElementChild.remove();
         };
 
         document.querySelector(".item .desc").appendChild(newList);
     })
-})
-
-document.querySelector(".item .desc").addEventListener("keypress", (k) => {
-    if (k.key == "Enter" && window.getSelection().anchorOffset > 0) {
-        let newList;
-        if (window.getSelection().focusNode.parentElement.nodeName == "LABEL") {
-            // let listPos = window.getSelection().focusNode.parentElement.parentElement.previousElementSibling;
-
-            // let newInput = listPos.firstElementChild.cloneNode(true);
-            // let newLabel = listPos.lastElementChild.cloneNode(true);
-            // newInput.removeAttribute("checked");
-            // newLabel.innerHTML = window.getSelection().focusNode.parentElement.innerHTML.slice(window.getSelection().anchorOffset, 9999999);
-
-            // listPos.appendChild(document.createElement("br"));
-            // listPos.appendChild(newInput);
-            // listPos.appendChild(newLabel);
-
-            // setTimeout(() => {
-            //     // window.getSelection().focusNode.parentElement.parentElement.remove();
-            // }, 100);
-        }
-        else if (window.getSelection().focusNode.parentElement.nodeName == "UL" || window.getSelection().focusNode.parentElement.nodeName == "OL") {
-            newList = document.createElement("li");
-            newList.innerHTML = window.getSelection().focusNode.parentElement.innerHTML.slice(window.getSelection().anchorOffset, 9999999);
-            window.getSelection().focusNode.parentElement.appendChild(newList);
-        }
-    }
-})
-
-document.querySelectorAll("nav .createItem button").forEach((e) => {
-    if (e.classList[0] == "import") {
-        e.addEventListener("click", () => {
-            let list = prompt(`Import a list from a different session. Warning: All current items will be deleted.`);
-            try {
-                if (list.split("§")[0].length > 0 || list.split("§")[1].length > 0) {
-                    document.querySelectorAll(".general-items tr, .calendar-date").forEach((tr) => tr.remove());
-                    init(list);
-                }
-            }
-            catch (err) {
-                alert("Import attempt failed, please double check your code is valid");
-                alert(err);
-            };
-        });
-    };
-    if (e.classList[0] == "export") {
-        e.addEventListener("click", () => {
-            let items = "";
-            document.querySelectorAll("nav table > tr, nav tbody > tr").forEach((tr) => {
-                tr.querySelectorAll("td").forEach((td) => {
-                    items += td.innerHTML;
-                    
-                    if (td.innerHTML != td.parentElement.lastElementChild.innerHTML || tr.parentElement.parentElement.firstElementChild.nodeName == "THEAD")
-                        items += "§";
-                })
-
-                if (tr.parentElement.parentElement.firstElementChild.nodeName == "THEAD")
-                    items += tr.parentElement.parentElement.firstElementChild.innerText.trimEnd(" ");
-
-
-                if (tr.innerHTML != document.querySelectorAll("nav table > tr, nav tbody > tr")[
-                    document.querySelectorAll("nav table > tr, nav tbody > tr").length - 1 ].innerHTML)
-                items += "|";
-            });
-            if (!navigator.clipboard) {
-                alert("Copying failed, try to import to a different session manually with the code: " + items);
-                return;
-            }
-            navigator.clipboard.writeText(items)
-            alert("Copy successfull, go to a different session and import the code to get your list.");
-        });
-    };
-})
+});
